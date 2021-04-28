a7b9e902bc1d85b275bffcd8d124a5137151f0e3
jhs512 <jangka512@gmail.com>
2021-04-28 10:44:45 +0900

    여태까지 구현한 내용 총정리, 주석

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -34,14 +34,29 @@ fun main() {
     println("== SIMPLE SSG 끝 ==")
 }
 
+// Rq는 UserRequest의 줄임말이다.
+// Request 라고 하지 않은 이유는, 이미 선점되어 있는 클래스명 이기 때문이다.
 class Rq(command: String) {
+    // 데이터 예시
+    // 전체 URL : /artile/detail?id=1
+    // actionPath : /artile/detail
     val actionPath: String
+
+    // 데이터 예시
+    // 전체 URL : /artile/detail?id=1&title=안녕
+    // paramMap : {id:"1", title:"안녕"}
     private val paramMap: Map<String, String>
 
+    // 객체 생성시 들어온 command 를 ?를 기준으로 나눈 후 추가 연산을 통해 actionPath와 paramMap의 초기화한다.
+    // init은 객체 생성시 자동으로 딱 1번 실행된다.
     init {
+        // ?를 기준으로 둘로 나눈다.
         val commandBits = command.split("?", limit = 2)
 
+        // 앞부분은 actionPath
         actionPath = commandBits[0].trim()
+
+        // 뒷부분이 있다면
         val queryStr = if (commandBits.lastIndex == 1 && commandBits[1].isNotEmpty()) {
             commandBits[1].trim()
         } else {
